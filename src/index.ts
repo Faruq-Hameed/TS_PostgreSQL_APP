80158fbb818a45457dc902091815522622c3f572
Faruq-Hameed <faruqhameed1@gmail.com>
2024-02-05 21:22:09 +0100

    added config variables to .env file

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,26 @@
 import express, { Application, Request, Response, NextFunction } from 'express';
+/** module to create a pool of connections.*/
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config()
 
 const app: Application = express();
-const port: number = 3000;
+// const port: number = 3000;
+const port: number = process.env.POOL_PORT;
+const poolPort = process.env.POOL_PORT;
+const poolPassword = process.env.POOL_PORT;
+
+//Pool configuration
+const pool: Pool = new Pool({
+    user: 'me',
+    host: 'localhost',
+    database:'api',
+    password: 'password',
+    port: 5432
+})
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
