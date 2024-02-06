3ad5745a770734a8b15ae7bb8114c92a8c26efb4
Faruq-Hameed <faruqhameed1@gmail.com>
2024-02-06 18:27:25 +0100

    added more apis

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import morgan from 'morgan';
 import dotenv from 'dotenv';
 import { router as userRouter } from './routes/userRouters'
 
-
 // Load environment variables from .env file
 dotenv.config()
 
@@ -14,11 +13,11 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use('/api', userRouter)
+
 app.get('/', (req: Request, res: Response) => {
     res.json({ info: 'Node.js, Express, and Postgres API' })
 })
-
-app.use('/api', userRouter)
 
 app.listen(port, () => {
     console.log('listening on port ' + port);
--- src/query.ts
+++ src/query.ts
@@ -1,12 +1,41 @@
 import express, { Application, Request, Response, NextFunction } from 'express';
 import pool from './utils/pool';
 
-
-export const getUsers = (req: Request, res: Response) =>{
-    pool.query('SELECT * FROM users ORDER BY id ASC', (error, results):void =>{
-        if(error){
+/** get all users api */
+export const getUsers = (req: Request, res: Response) => {
+    pool.query('SELECT * FROM users ORDER BY id ASC', (error, results): void => {
+        if (error) {
             throw error;
         }
         res.status(200).json(results.rows);
     })
+}
+
+/** get all users api */
+export const getUserById = (req: Request, res: Response): void => {
+    const id: number = parseInt(req.params.id)
+    pool.query('SELECT * FROM users WHERE id = $1', [id], (error, results) => {
+        if (error) {
+            throw error
+        }
+        res.status(200).json(results.rows);
+    })
+}
+
+/**Create new user */
+export const createUser = (req: Request, res: Response): void => {
+    const { name, email } = req.body;
+    pool.query('INSERT INTO users (name, email) VALUES ($1, $2) RETURNING *',
+        [name, email], (error, results) => {
+            if (error) {
+                throw error
+            }
+            res.
+            status(200)
+            .send(
+                {message: `User added with ID: ${results.rows[0].id}`, 
+                results: results.rows[0]
+            })
+        }
+    )
 }
