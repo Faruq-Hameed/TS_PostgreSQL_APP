8ed35dc9cb449ece34ba6ef25835bc29c1a7ba4b
Faruq-Hameed <faruqhameed1@gmail.com>
2024-02-07 18:28:50 +0100

    added cors and error handlers

--- dist/index.js
+++ dist/index.js
@@ -6,18 +6,31 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const morgan_1 = __importDefault(require("morgan"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const cors_1 = __importDefault(require("cors")); // to block requests from different origins
 const userRouters_1 = require("./routes/userRouters");
 // Load environment variables from .env file
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = parseInt(process.env.PORT || '3030');
+app.use((0, cors_1.default)()); //enable CORS for all origins.
 app.use((0, morgan_1.default)('dev'));
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
 app.use('/api', userRouters_1.router);
-app.get('/', (req, res) => {
-    res.json({ info: 'Node.js, Express, and Postgres API' });
+app.get('*', (req, res) => {
+    res.status(400).json({ message: 'Invalid endpoint' });
 });
+//error handler
+app.use((req, res, next) => {
+    const error = new Error('Something went wrong');
+    next(error);
+});
+// Error-handling Middleware
+const errorMiddleware = (err, req, res, next) => {
+    console.error('Error:', err.message);
+    res.status(500).send('Internal Server Error');
+};
+app.use(errorMiddleware);
 app.listen(port, () => {
     console.log('listening on port ' + port);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
-import express, { Application, Request, Response, NextFunction } from 'express';
+import express, { Application, Request, Response, NextFunction, ErrorRequestHandler } from 'express';
 import morgan from 'morgan';
 import dotenv from 'dotenv';
+import cors from 'cors'; // to block requests from different origins
 import { router as userRouter } from './routes/userRouters'
 
 // Load environment variables from .env file
@@ -9,15 +10,30 @@ dotenv.config()
 const app: Application = express();
 const port: number = parseInt(process.env.PORT || '3030' as string);
 
+app.use(cors()); //enable CORS for all origins.
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/api', userRouter)
 
-app.get('/', (req: Request, res: Response) => {
-    res.json({ info: 'Node.js, Express, and Postgres API' })
+app.get('*', (req: Request, res: Response) => {
+    res.status(400).json({ message: 'Invalid endpoint' })
 })
+//error handler
+app.use((req, res, next) => {
+    const error : Error = new Error('Something went wrong')
+    next(error)
+})
+
+// Error-handling Middleware
+const errorMiddleware: ErrorRequestHandler = (err, req: Request, res : Response, next) => {
+    console.error('Error:', err.message);
+    res.status(500).send('Internal Server Error');
+}
+app.use(errorMiddleware)
+
+
 
 app.listen(port, () => {
     console.log('listening on port ' + port);
