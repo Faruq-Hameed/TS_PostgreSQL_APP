5911fca3ffd0890eb94694df631ff226437d3635
Faruq-Hameed <faruqhameed1@gmail.com>
2024-02-07 07:43:44 +0100

    fixing upsdateUser controller

--- src/query.ts
+++ src/query.ts
@@ -63,8 +63,8 @@ export const updateUser = (req: Request, res: Response) => {
         }
         //if a user is found return the updated user
         res.status(200).send({
-                    message: `User modified with ID: ${id}`,
-                    results: results.rows[0]
-                })
+            message: `User modified with ID: ${id}`,
+            results: results.rows[0]
+        })
     })
 }
