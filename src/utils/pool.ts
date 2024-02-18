1bcb2f7b9795ec8a9e2877ecd2dfdf21edd67817
Faruq-Hameed <faruqhameed1@gmail.com>
2024-02-18 11:09:07 +0100

    removed redundant codes

--- src/utils/pool.ts
+++ src/utils/pool.ts
@@ -13,13 +13,12 @@ const pool: Pool = new Pool({
     user: 'me',
     host: 'localhost',
     database:'api',
-    password: poolPassword,
-    port: parseInt(poolPort as string)
+    password: process.env.POOL_PASSWORD,
+    port: parseInt(process.env.POOL_PORT as string)
 })
 
-// Export the pool configuration
+// Export the pool configuration(though not required)
 pool.connect().then(()=>{
     console.log('connected to pg successfully');
 })
 export default pool
-
